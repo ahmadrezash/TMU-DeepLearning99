c92b3aedaba1f90c47dac32bea8f4e9931136eb2
ahmad-scarf-work <ahmad.sharif.abc@gmail.com>
2021-04-08 15:59:38 +0430

    without report

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
-import torch
-from torch.autograd import Variable
 import matplotlib.pyplot as plt
+import torch
+from sklearn.model_selection import train_test_split
+from torch.autograd import Variable
 
 
 class Trainer:
@@ -12,10 +13,16 @@ class Trainer:
 		self.criterion = criterion or torch.nn.MSELoss()
 		self.optimizer = optimizer or torch.optim.SGD(model.parameters(), lr=0.01)
 		self.loss_history = []
+		self.valid_loss_history = []
 
-	def fit(self, x_train, y_train):
+	def fit(self, X, Y):
+		x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=.5)
+
 		x_train = Variable(torch.tensor(x_train, dtype=torch.float))
 		y_train = Variable(torch.tensor(y_train, dtype=torch.float))
+
+		x_test = Variable(torch.tensor(x_test, dtype=torch.float))
+		y_test = Variable(torch.tensor(y_test, dtype=torch.float))
 
 		epoch = self.epoch
 		model = self.model
@@ -23,6 +30,13 @@ class Trainer:
 		optimizer = self.optimizer
 
 		self.loss_history = []
+		self.valid_loss_history = []
+
+		# Validation Step1
+		y_pred = model(x_test)
+		loss = criterion(y_pred.squeeze(), y_test)
+		self.valid_loss_history.append(loss)
+
 		for i in range(epoch):
 			optimizer.zero_grad()
 
@@ -39,5 +53,14 @@ class Trainer:
 			loss.backward()
 			optimizer.step()
 
+			# Validation
+			y_pred = model(x_test)
+			loss = criterion(y_pred.squeeze(), y_test)
+			self.valid_loss_history.append(loss)
+
+		self.plot_loss()
+
 	def plot_loss(self):
-		plt.plot(self.loss_history)
+		plt.plot(self.loss_history, label="train_data")
+		plt.plot(self.valid_loss_history, label="test_data")
+		plt.legend()
